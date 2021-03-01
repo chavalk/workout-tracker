e65b46d47e29e5a8b45cf37fc5346ea89684c9ea
chavalk <chavalk@hotmail.com>
2021-02-28 23:51:58 -0600

    Add routes to get all workouts and to find an update by ID.

---
+++ routes/apiroutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts/:id", ({ body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
