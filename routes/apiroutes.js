2434bf678e00caac45e9a12f50a92227852a81e9
chavalk <chavalk@hotmail.com>
2021-03-02 22:13:24 -0600

    Deploying.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,13 +2,19 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
 });
 
 router.put("/api/workouts/:id", ({ body, params}, res) => {
@@ -45,7 +51,6 @@ router.get("/api/workouts/range", (req, res) => {
     ])
     .sort({ _id: -1 }).limit(7)
     .then(dbWorkout => {
-        
         res.json(dbWorkout);
     })
     .catch(err => {
